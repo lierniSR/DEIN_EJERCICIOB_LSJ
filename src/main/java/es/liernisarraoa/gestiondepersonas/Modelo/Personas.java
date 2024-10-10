28b599033556045100428159e9919bb6f7491b50
Lierni <lierni@gmail.com>
2024-10-10 14:20:16 +0200

    Clase Persona creada

---
+++ src/main/java/es/liernisarraoa/gestiondepersonas/Modelo/Personas.java
@@ -0,0 +1,38 @@
+package es.liernisarraoa.gestiondepersonas.Modelo;
+
+public class Personas {
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+
+    public Personas(String nombre, String apellido, Integer edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    //Getters and setters
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public Integer getEdad(){
+        return edad;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public void setEdad(Integer edad){
+        this.edad = edad;
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module es.liernisarraoa.gestiondepersonas {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.liernisarraoa.gestiondepersonas to javafx.fxml;
+    exports es.liernisarraoa.gestiondepersonas;
+    exports es.liernisarraoa.gestiondepersonas.Controlador;
+    opens es.liernisarraoa.gestiondepersonas.Controlador to javafx.fxml;
+}
