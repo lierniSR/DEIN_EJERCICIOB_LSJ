6e8952cadcc68ba3160a69a2a3bd4aa387bbd36c
Lierni <lierni@gmail.com>
2024-10-11 16:47:06 +0200

    Scene Builder creado

--- src/main/java/es/liernisarraoa/gestiondepersonas/Controlador/HelloController.java
+++ src/main/java/es/liernisarraoa/gestiondepersonas/Controlador/HelloController.java
@@ -1,14 +1,33 @@
 package es.liernisarraoa.gestiondepersonas.Controlador;
 
+import es.liernisarraoa.gestiondepersonas.Modelo.Personas;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TableView<Personas> tablaPersonas;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<Personas, String> columnaNombre;
+
+    @FXML
+    private TableColumn<Personas, String> columnaApellido;
+
+    @FXML
+    private TableColumn<Personas, String> columnaEdad;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tablaPersonas.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+        columnaNombre.setResizable(true);
+        columnaApellido.setResizable(true);
+        columnaEdad.setResizable(true);
     }
 }
