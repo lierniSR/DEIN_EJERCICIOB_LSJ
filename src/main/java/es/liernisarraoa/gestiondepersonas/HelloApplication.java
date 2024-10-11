20a7848ebb1df3bd1d9dc5cbef04dfa62a2bffd8
Lierni <lierni@gmail.com>
2024-10-11 21:50:03 +0200

    HelloAplication.java comentado

--- src/main/java/es/liernisarraoa/gestiondepersonas/HelloApplication.java
+++ src/main/java/es/liernisarraoa/gestiondepersonas/HelloApplication.java
@@ -9,26 +9,50 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * Clase principal de la aplicación.
+ *
+ * @author Lierni
+ * @version 1.0
+ */
 public class HelloApplication extends Application {
     private static Scene scene;
 
+    /**
+     * Inicializa la aplicación y configura la escena.
+     *
+     * @param stage la escena principal de la aplicación
+     * @throws IOException si ocurre un error al cargar el archivo FXML
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gestionDePersona.fxml"));
         scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Gestion de Personas");
+        /**
+         * Insertar un icono a la escena, la carpeta de las Imagenes tiene que estar dentro de resources
+         */
         stage.getIcons().add(new Image(String.valueOf(HelloApplication.class.getResource("/Imagenes/agenda.png"))));
         stage.setResizable(true);
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Punto de entrada de la aplicación.
+     *
+     * @param args los argumentos de la línea de comandos
+     */
     public static void main(String[] args) {
         launch();
     }
 
+    /**
+     * Obtiene la escena principal de la aplicación.
+     *
+     * @return la escena principal
+     */
     public static Scene getScene(){
         return scene;
     }
-    //Falta el icono
 }
